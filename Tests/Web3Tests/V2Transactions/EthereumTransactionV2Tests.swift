3c049b148ff9b75d25552e9ddb5f2754982dde52
Andrew Podkovyrin <podkovyrin@gmail.com>
2021-08-21 15:18:44 +0300

    Retry HTTP requests in case of any failure

--- Tests/Web3Tests/V2Transactions/EthereumTransactionV2Tests.swift
+++ Tests/Web3Tests/V2Transactions/EthereumTransactionV2Tests.swift
@@ -12,6 +12,33 @@ import PromiseKit
 import Web3PromiseKit
 
 class EthereumTransactionV2Tests: XCTestCase {
+    func testEstimation() throws {
+        let call = EthereumCall(
+            from: try EthereumAddress(hex: "0x1d23118D0Dd260547610b5326C2E62bE7F5f6fAa", eip55: true),
+            to: try EthereumAddress(hex: "0x083fc10cE7e97CaFBaE0fE332a9c4384c5f54E45", eip55: true),
+            maxPriorityFeePerGas: EthereumQuantity(quantity: 10.gwei),
+            maxFeePerGas: EthereumQuantity(quantity: 80.gwei),
+            value: EthereumQuantity(quantity: 1337),
+            data: EthereumData([])
+        )
+
+        let web3 = Web3(rpcURL: "https://ropsten.infura.io/v3/0cbb4b8535bc4a928548b4d16ccf6bfa")
+
+        let exp = expectation(description: "call is estimated")
+        web3.eth.estimateGas(call: call, block: .latest) { response in
+            switch response.status {
+            case .success(let quantity):
+                XCTAssertEqual(quantity, EthereumQuantity(quantity: 21000))
+            case .failure(let error):
+                XCTFail("\(error)")
+            }
+
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 60)
+    }
+
     func testV2TransactionSignature() throws {
         let privateKey = try! EthereumPrivateKey(hexPrivateKey: "0xdf09d380d685a8233cda012eae34caa49e2a74e0ee1406fe6004b60144410a19")
 
