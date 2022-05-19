60a775ba201c4d3f106969553a2dffe8eed5c8e0
Andrew Podkovyrin <podkovyrin@gmail.com>
2022-05-19 21:24:54 +0200

    Bump swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 
 import PackageDescription
 
@@ -17,7 +17,7 @@ let package = Package(
         // Core dependencies
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.1"),
-        .package(name: "secp256k1", url: "https://github.com/1inch/secp256k1.swift", from: "0.2.2"),
+        .package(url: "https://github.com/1inch/secp256k1.swift", from: "0.2.2"),
 
         // PromiseKit dependency
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.15.3"),
@@ -32,7 +32,7 @@ let package = Package(
             dependencies: [
                 .product(name: "BigInt", package: "BigInt"),
                 .product(name: "CryptoSwift", package: "CryptoSwift"),
-                .product(name: "secp256k1", package: "secp256k1"),
+                .product(name: "secp256k1", package: "secp256k1.swift"),
             ],
             path: "Sources/Core"
         ),
